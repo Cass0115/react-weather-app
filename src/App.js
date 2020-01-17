e9d5e2c64101f5fe9c821cd0ab3d498d30b0445e
Cassie Durr <Cass0115@gmail.com>
2020-01-17 15:37:32 -0600

    update dep

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div class="wrapper"> 
+//         <h1>Weather</h1> 
+
+//       </div>
+//     );
+//   }
+// }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>New paragraph!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
